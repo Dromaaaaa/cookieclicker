40fd7d179f9f783048708dc0e8d6152de5016a04
Mehdi <mahdi.benjebara@ecoles-epsi.net>
2025-02-05 14:49:10 +0100

    Finished the development, added an image for the cookie, and added the functionnality for Click on Cookie + Cookie counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import './App.css'
-
+import { useState } from "react"
 const App = () => {
 
+  const [cookies, setCookies] = useState(0)
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="w-screen h-screen flex">
+
+      <div className="flex-1 flex justify-center items-center">
+        <p>Total cookies : {cookies}</p>
+        <img
+          onClick={() => { setCookies(cookies + 1) }}
+          className="h-100" src="https://sweetlorens.com/cdn/shop/products/Copy-of-Chocolate-Chunk-Full-Cookie-transparent-background.png" alt="cookie-logo" />
+      </div>
+
+      <div className="flex-1 bg-red-200">items</div>
+      <div className="flex-1 bg-yellow-200">shop</div>
+
+    </div>
   )
 }
 
+export default App
